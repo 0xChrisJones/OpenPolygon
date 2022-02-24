9a49f75736dde50b536b777f3c7b804ffbbf6ab6
0xChrisJones <0xchrisjones@gmail.com>
2022-02-24 19:40:43 +0100

    Initial commit

---
+++ client/pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>OpenPolygon NFT Marketplace</title>
+        <meta name="description" content="NFT Marketplace build on Polygon" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+  )
+}
+
+export default Home
