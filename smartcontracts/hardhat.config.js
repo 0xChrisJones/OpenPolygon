d2fcab1497d51fab4dd750c77e0a5b23103999c3
0xChrisJones <0xchrisjones@gmail.com>
2022-02-25 21:49:02 +0100

    Added config

--- smartcontracts/hardhat.config.js
+++ smartcontracts/hardhat.config.js
@@ -1,8 +1,15 @@
 require('@nomiclabs/hardhat-waffle');
+require('dotenv').config({ path: '.env' });
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: '0.8.4',
+  networks: {
+    mumbai: {
+      url: process.env.ALCHEMY_API_KEY_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
