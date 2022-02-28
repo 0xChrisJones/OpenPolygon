a7c2dc18882abc131c3e555f6b0ee9372e8245e5
0xChrisJones <0xchrisjones@gmail.com>
2022-02-28 19:57:54 +0100

    Updated NFT Marketplace contract

--- smartcontracts/contracts/NFTMarketplace.sol
+++ smartcontracts/contracts/NFTMarketplace.sol
@@ -12,6 +12,38 @@ contract NFTMarketplace is ReentrancyGuard {
   Counters.Counter private _itemIds;
   Counters.Counter private _itemsSold;
 
+  uint256 listingFee = 0.025 ether;
   address payable owner;
-   
+
+  mapping(uint256 => MarketItem) private idToMarketItem;
+  
+  struct MarketItem {
+    uint256 tokenID;
+    address payable seller;
+    address payable owner;
+    uint256 price;
+    bool sold;
+  }
+
+  event MarketItemCreated (
+    uint256 indexed tokenId,
+    address seller,
+    uint256 price
+  );
+
+  constructor() {  
+    owner = payable(msg.sender);
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner, "Only owner can access this functionality");
+    _;
+  }
+
+  function updateListingFee(uint256 _listingFee) public onlyOwner {
+    listingFee = _listingFee;
+  }
+  function getListingFee() public view returns (uint256) {
+    return listingFee;
+  }
 }
