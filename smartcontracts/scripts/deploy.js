9f790e710b78891c5cadaca439b8f35c91b55359
0xChrisJones <0xchrisjones@gmail.com>
2022-03-02 18:55:47 +0100

    Added deploy script

--- smartcontracts/scripts/deploy.js
+++ smartcontracts/scripts/deploy.js
@@ -1,6 +1,18 @@
 const { ethers } = require('hardhat');
 
-async function main() {}
+async function main() {
+  const NFTMarketplaceContract = await ethers.getContractFactory(
+    'NFTMarketplace'
+  );
+
+  const deployedContract = await NFTMarketplaceContract.deploy();
+  await deployedContract.wait();
+
+  console.log(
+    'NFT Marketplace Contract deployed on address: ',
+    deployedContract.address
+  );
+}
 
 main()
   .then(() => process.exit(0))
