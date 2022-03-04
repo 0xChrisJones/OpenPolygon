f5da9dcac51d3dbad77e28b817e4e0e086bb18c9
0xChrisJones <0xchrisjones@gmail.com>
2022-03-04 19:28:00 +0100

    Added core contract functionality

--- smartcontracts/contracts/NFTMarketplace.sol
+++ smartcontracts/contracts/NFTMarketplace.sol
@@ -16,7 +16,7 @@ contract NFTMarketplace is ERC721URIStorage, ReentrancyGuard {
   uint256 listingFee = 0.025 ether;
   address payable owner;
 
-  mapping(uint256 => MarketItem) private idToMarketItem;
+  mapping(uint256 => MarketItem) private idToToken;
   
   struct MarketItem {
     uint256 tokenID;
@@ -32,6 +32,11 @@ contract NFTMarketplace is ERC721URIStorage, ReentrancyGuard {
     uint256 price
   );
 
+  event MarketItemSale (
+    uint256 tokenId,
+    address owner
+  );
+
   constructor() ERC721("OpenPolygon Tokens", "OPT") {  
     owner = payable(msg.sender);
   }
@@ -41,40 +46,117 @@ contract NFTMarketplace is ERC721URIStorage, ReentrancyGuard {
     _;
   }
 
+  function getTokensSoldCount() public view returns (uint256) {
+    return _tokensSold.current();
+  }
+
   function updateListingFee(uint256 _listingFee) public onlyOwner {
     listingFee = _listingFee;
   }
+
   function getListingFee() public view returns (uint256) {
     return listingFee;
   }
 
   function createAndListItem(string memory tokenURI, uint256 price) public payable nonReentrant returns (uint256) {
+    require(price > 0.1 ether, "Price must be at least 0.1 MATIC");
+    require(msg.value >= listingFee, "Listing fee not sufficient");
+
     _tokenIds.increment();
     uint256 newTokenID = _tokenIds.current();
 
     _mint(msg.sender, newTokenID);
     _setTokenURI(newTokenID, tokenURI);
-    listItem(newTokenID, price);
-    return newTokenID;
-  }
 
-  function listItem(uint256 itemID, uint256 price) private {
-    require(price > 0.1 ether, "Price must be at least 0.1 MATIC");
-    require(msg.value >= listingFee, "Listing fee not sufficient");
-
-    idToMarketItem[itemID] = MarketItem(
-      itemID,
+    idToToken[newTokenID] = MarketItem(
+      newTokenID,
       payable(msg.sender),
       payable(address(this)),
       price,
       false
     );
+    _transfer(msg.sender, address(this), newTokenID);
 
-    _transfer(msg.sender, address(this), itemID);
     emit MarketItemCreated(
-      itemID,
+      newTokenID,
       msg.sender,
       price
     );
+    return newTokenID;
+  }
+
+  function createItemSale(uint256 tokenID) public payable nonReentrant {
+    uint256 price = idToToken[tokenID].price;
+    require(price == msg.value, "Funds provided do not cover the token price");
+    idToToken[tokenID].owner = payable(msg.sender);
+    idToToken[tokenID].sold = true;
+    idToToken[tokenID].seller = payable(address(0));
+    _tokensSold.increment();
+    _transfer(address(this), msg.sender, tokenID);
+    payable(idToToken[tokenID].seller).transfer(msg.value);
+
+    emit MarketItemSale(
+      tokenID,
+      msg.sender
+    );
+  }
+
+  function fetchUnsoldItems() public view returns (MarketItem[] memory) {
+    uint256 itemsCount = _tokenIds.current();
+    uint256 itemsSold = _tokensSold.current();
+    uint256 unsoldItemCount = itemsCount - itemsSold;
+
+    MarketItem[] memory unsoldItems = new MarketItem[](unsoldItemCount);
+
+    for (uint256 i = 0; i < itemsCount; i++) {
+      if(idToToken[i+1].owner == payable(address(this))) {
+        MarketItem memory currentItem = idToToken[i + 1];
+        unsoldItems[i] = currentItem;
+      }
+    }
+    return unsoldItems;
+  }
+
+  function fetchMyItems() public view returns (MarketItem[] memory) {
+    uint256 itemsCount = _tokenIds.current();
+    uint256 ownedItemsCount = 0;
+
+    for (uint256 i = 0; i < itemsCount; i++) {
+      if(msg.sender == idToToken[i + 1].owner) 
+      ownedItemsCount += 1;
+    }
+
+    MarketItem[] memory ownedItems = new MarketItem[](ownedItemsCount);
+
+    for (uint256 i = 0; i < itemsCount; i++) {
+      uint256 currentID = i + 1; 
+
+      if(msg.sender == idToToken[currentID].owner) 
+      {
+        MarketItem memory currentItem = idToToken[currentID];
+        ownedItems[i] = currentItem;
+      }
+    }
+    return ownedItems;
+  }
+
+  function fetchListedItems() public view returns (MarketItem[] memory) {
+    uint256 itemsCount = _tokenIds.current();
+    uint256 listedItemsCount = 0;
+
+    for (uint256 i = 0; i < itemsCount; i++)
+      if (idToToken[i + 1].seller == payable(msg.sender)) 
+        listedItemsCount += 1;
+    
+    MarketItem[] memory listedItems = new MarketItem[](listedItemsCount);
+
+    for (uint256 i = 0; i < itemsCount; i++) {
+      uint256 currentID = i + 1; 
+      if (idToToken[currentID].seller == payable(msg.sender)) {
+        MarketItem memory currentItem = idToToken[currentID];
+        listedItems[i] = currentItem;
+      }
+    }
+    return listedItems;
   }
 }
