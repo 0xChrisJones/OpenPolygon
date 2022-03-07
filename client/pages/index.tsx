99c40bca6c86554a02efb13e967e3a014ae08368
0xChrisJones <0xchrisjones@gmail.com>
2022-03-07 20:13:12 +0100

    Created website layout

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,22 +1,18 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>OpenPolygon NFT Marketplace</title>
         <meta name="description" content="NFT Marketplace build on Polygon" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-      </main>
+      <div></div>
+    </>
+  );
+};
 
-      <footer>
-      </footer>
-    </div>
-  )
-}
-
-export default Home
+export default Home;
