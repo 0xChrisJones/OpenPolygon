705942e045464fe28f74ccac467116c954e4cae0
0xChrisJones <0xchrisjones@gmail.com>
2022-03-11 11:30:20 +0100

    Added theme colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blueMain: '#8247e5',
+      },
+    },
   },
   plugins: [],
 };
