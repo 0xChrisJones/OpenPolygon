4df64ecaedd21f483845c43690502f7dad06ce2c
0xChrisJones <0xchrisjones@gmail.com>
2022-03-12 20:39:57 +0100

    Added a simple react context

---
+++ client/context/OpenPolygonContext.tsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+
+interface IOpenPolygonContext {
+  account: string;
+  connectWallet: () => void;
+}
+
+export const OpenPolygonContext = React.createContext<
+  Partial<IOpenPolygonContext>
+>({});
+
+export const OpenPolygonProvider: React.FC = ({ children }) => {
+  const [account, setAccount] = useState('');
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      if (!(window as any).ethereum) alert('Please install Metamask!');
+
+      const accounts = await (window as any).ethereum.request({
+        method: 'eth_accounts',
+      });
+
+      if (accounts.length) {
+        setAccount(accounts[0]);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  const connectWallet = async () => {
+    try {
+      if (!(window as any).ethereum) alert('Please install Metamask!');
+
+      const accounts = await (window as any).ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+
+      if (accounts.length) {
+        setAccount(accounts[0]);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <OpenPolygonContext.Provider value={{ account, connectWallet }}>
+      {children}
+    </OpenPolygonContext.Provider>
+  );
+};
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,15 +2,18 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { OpenPolygonProvider } from '../context/OpenPolygonContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Navbar />
-      <main>
-        <Component {...pageProps} />
-      </main>
-      <Footer />
+      <OpenPolygonProvider>
+        <Navbar />
+        <main>
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </OpenPolygonProvider>
     </>
   );
 }
